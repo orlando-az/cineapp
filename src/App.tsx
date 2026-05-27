808cc309b1c2bb2e77a0370f18fb2f9d84a3355d
Orlando Isaac Aguilera <tj.orlando.aguilera.z@upds.net.bo>
2026-05-26 21:41:05 -0400

    Add Supabase integration and update App component to fetch movie data

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,19 @@
+import { useEffect } from "react";
 import Catalog from "./page/Catalog";
+import { supabase } from "./lib/supabaseClient";
 
 const App = () => {
+  useEffect(() => {
+    const probar = async () => {
+      const { data, error } = await supabase.from("peliculas").select("*");
+
+      console.log(data);
+      console.log(error);
+    };
+
+    probar();
+  }, []);
+
   return <Catalog />;
 };
 
